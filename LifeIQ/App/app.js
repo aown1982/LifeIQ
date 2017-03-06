6e047ac62a7c0bce782b0ca970be505495bd055c
Syed Aown <aown1982@gmail.com>
2017-03-06 18:37:12 +1100

    Initial Setup

---
+++ LifeIQ/App/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    var app=  angular.module('iqApp', ['ngRoute']).config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('xmlHttpInteceptor');
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    }]);
+
+    function errorHandler(status, message) {
+        var scope = angular.element($('html')).scope();
+        scope.errorHandler(status, message);
+    };;
+
+
+})();
---
+++ LifeIQ/App/routeConfig.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('iqApp')
+        .config([
+            '$routeProvider', function($routeProvider) {
+                $routeProvider.when('/',
+                    {
+                        templateUrl: '/App/Templates/Contact/Index.html',
+                        requiresLogin: true,
+                        controller: 'ContactController'
+                    })
+                    .when('/Account/Login',
+                    {
+                        templateUrl: '/App/Templates/Account/Login.html',
+                        controller: 'LoginController'
+                    })
+                    .when('/Account/Register',
+                    {
+                        templateUrl: '/App/Templates/Account/Register.html',
+                        controller: 'RegisterController'
+                    })
+                    .otherwise({
+                        templateUrl: '/App/Templates/Shared/_404.html'
+                    });
+            }
+        ]);
+       
+})();
